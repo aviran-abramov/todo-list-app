bd7d741eaf88ca8462174f664562d79409d7ad14
Aviran Abramov <aviranabramov@gmail.com>
2023-01-21 19:37:25 +0200

    Transfer the header from the main file to a new component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,15 @@
 <template>
-  <header>
-    <h1 class="logo">Todo App</h1>
-  </header>
+  <TodoHeader />
 </template>
 
 <script>
-export default {};
+import TodoHeader from "./components/TodoHeader.vue";
+
+export default {
+  components: {
+    TodoHeader,
+  },
+};
 </script>
 
-<style scoped>
-header {
-  background-color: #007bff;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 80px;
-}
-
-.logo {
-  color: #fff;
-  font-weight: bold;
-  font-size: 2.4rem;
-}
-</style>
+<style scoped></style>
---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <h1 class="logo">Todo App</h1>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+header {
+  background-color: #007bff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80px;
+}
+
+.logo {
+  color: #fff;
+  font-weight: bold;
+  font-size: 2.4rem;
+}
+</style>
