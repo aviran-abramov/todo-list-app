2dc2d5948b08a05f5beba05c1e630d31d8b595ff
Aviran Abramov <aviranabramov@gmail.com>
2023-01-21 20:26:30 +0200

    Transfer the new task container into a new component

---
+++ src/components/NewTask.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="add-task-container container">
+    <h3>New Task</h3>
+    <form @submit.prevent="addTask">
+      <input v-model="enteredNewTask" type="text" />
+      <button>+</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      enteredNewTask: "",
+    };
+  },
+  methods: {
+    addTask() {
+      this.enteredNewTask = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+* {
+  padding: 0;
+}
+.add-task-container {
+  max-width: 400px;
+}
+.container {
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.25);
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 20px;
+  padding: 13px;
+  border-radius: 6px;
+}
+
+h3 {
+  font-weight: bold;
+  font-size: 1.2rem;
+}
+
+form {
+  display: flex;
+}
+
+form input {
+  width: 250px;
+  height: 30px;
+  padding: 6px;
+}
+
+form button {
+  background-color: #007bff;
+  color: white;
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 40px;
+  height: 30px;
+  width: 30px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+}
+
+form button:hover {
+  transform: scale(1.15);
+}
+</style>
