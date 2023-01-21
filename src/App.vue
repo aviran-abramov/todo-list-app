b6954ac2d249be7e459c43a42f3938222ac519ae
Aviran Abramov <aviranabramov@gmail.com>
2023-01-21 19:33:40 +0200

    Create header for the app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
-  <h1>Todo App</h1>
+  <header>
+    <h1 class="logo">Todo App</h1>
+  </header>
 </template>
 
 <script>
 export default {};
 </script>
+
+<style scoped>
+header {
+  background-color: #007bff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80px;
+}
+
+.logo {
+  color: #fff;
+  font-weight: bold;
+  font-size: 2.4rem;
+}
+</style>
