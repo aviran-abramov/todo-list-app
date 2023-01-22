78645c9c296eff30b4932ce45da3f68b6ff7ecf9
Aviran Abramov <aviranabramov@gmail.com>
2023-01-22 06:34:06 +0200

    Make the X button remove the selected task by clicking on it

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <TodoHeader />
   <main>
     <NewTask @submit-task="addTask" />
+    <TasksView :tasks="tasks" @submit-task-removal="removeTask" />
   </main>
 </template>
 
 <script>
 import TodoHeader from "./components/TodoHeader.vue";
 import NewTask from "./components/NewTask.vue";
+import TasksView from "./components/TasksView.vue";
 
 export default {
   components: {
     TodoHeader,
     NewTask,
+    TasksView,
   },
   data() {
     return {
@@ -25,6 +28,10 @@ export default {
         id: this.tasks.length,
         text: task,
       });
+    },
+    removeTask(taskId) {
+      const selectedTask = this.tasks.find((task) => task.id === taskId);
+      this.tasks = this.tasks.filter((task) => task !== selectedTask);
     },
   },
 };
---
+++ src/components/TasksView.vue
@@ -0,0 +1,99 @@
+<template>
+  <div v-if="tasks.length > 0" class="container">
+    <h2>To-do List</h2>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        <p>{{ task.text }}</p>
+        <div class="buttons">
+          <button class="finish">V</button>
+          <button @click="submitTaskRemoval(task.id)" class="remove">X</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["submit-task-removal"],
+  props: {
+    tasks: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    submitTaskRemoval(taskId) {
+      this.$emit("submit-task-removal", taskId);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.25);
+  margin: 0 auto;
+  margin-top: 20px;
+  padding: 13px;
+  border-radius: 6px;
+  max-width: 90%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+h2 {
+  font-weight: bold;
+  font-size: 1.8rem;
+  margin-bottom: 10px;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+li {
+  background-color: rgb(240, 240, 240);
+  padding: 6px 10px;
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid black;
+  border-radius: 3px;
+}
+
+li p {
+  font-weight: bold;
+}
+
+.buttons {
+  display: flex;
+  gap: 10px;
+}
+
+button {
+  border: 2px solid black;
+  height: 26px;
+  width: 26px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+button:hover {
+  transform: scale(1.12);
+}
+
+.buttons .finish {
+  background-color: #01e901;
+}
+
+.buttons .remove {
+  background-color: #f72f35;
+}
+</style>
